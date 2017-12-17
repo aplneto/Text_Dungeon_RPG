fa0c07e915670d58027eb476d4badf28cf7f0e7b
Antônio Neto <34499237+aplneto@users.noreply.github.com>
2017-12-17 05:56:48 -0200

    Initial gameplay configuration
    I started editing the text that will be shown to the player to make the huge amount of text a bit less tiresome, also I still need to build the IAs, enemy selection menu and experience points menu but it is almos finnished

--- Joguinho.py/jogo.py
+++ Joguinho.py/jogo.py
@@ -4,7 +4,7 @@ Joguinho em 3D&T Alpha para praticar o uso de dicionários de Python.
 12-15-2017
 00:06
 
-Bem-vindo ao desafio de Valkaria, um RPG no cenário de Tormenta onde você enfrenta hordas de inimigos em um calabouço, em busca do Desbravador, a arma mágica da Deusa.
+Bem-vindo a masmorra de treinos do Protetorado do Reino, um RPG no cenário de Tormenta onde você enfrenta hordas de inimigos em um calabouço, em busca do Desbravador, a arma mágica da Deusa.
 
 Opções:
 CONTINUAR --> O personagem segue para enfrentar o próximo inimigo, escolhido aleatoriamente dentre um determinado número de inimigos.
@@ -13,72 +13,72 @@ DESISTIR --> Salva um arquivo de Score contendo a pontuação do jogador.
 
 Personagens:
 GUERREIRO --> Força 3, Habilidade 2, Resistência 3, Armadura 2, Poder de Fogo 0, Pontos de Vida 20, Pontos de Magia 10
-MAGO --> Força 1, Habilidade 3, Resistência 2, Armadura 0, Poder de Fogo 0, Pontos de Vida 10, Pontos de Magia 30
-PALADINO --> Força 2, Habilidade 3, Resistência 3, Armadura 2, Poder de Fogo 0, Pontos de Vida 15, Pontos de Magia 15
-ARQUEIRO --> Força 0, Habilidade 2, Resistência 2, Armadura 1, Poder de Fogo 3, Pontos de Vida 10, Pontos de Magia 10
+MAGO --> Força 1, Habilidade 3, Resistência 2, Armadura 1, Poder de Fogo 0, Pontos de Vida 10, Pontos de Magia 30
+PALADINO --> Força 2, Habilidade 2, Resistência 3, Armadura 2, Poder de Fogo 0, Pontos de Vida 15, Pontos de Magia 15
+ARQUEIRO --> Força 1, Habilidade 2, Resistência 2, Armadura 1, Poder de Fogo 3, Pontos de Vida 10, Pontos de Magia 10
 
 Os personagens podem usar as seguintes ações.
 GUERREIRO --> Machado; Ataque especial perigoso e poderoso (3 PMs)
 MAGO --> Cajado; Arpão (15 PMs), Bola de Fogo (5 PMs), O Crânio Voador de Vladislav (3 PMs)
 PALADINO --> Espada; Ataque Vorpal (3 PMs), Cura Mágica (2 PMs), Esconjuro (5 PMs)
-ARQUEIRO --> Arco; Tiro Preciso, Perigoso e Poderoso (4 PMs)
+ARQUEIRO --> Faca, Arco; Tiro Preciso, Perigoso e Poderoso (4 PMs)
 
 Já os inimigos são:
 '''
 from random import randint
 from masmorra import *
+from dummies import *
 
 def Main():
     '''
     Função principal do jogo.
     '''
-    PLAYER = {'Nome': 'jogador', 'F':0, 'H':0, 'R':0, 'A':0, 'PdF':0, 'PV':1, 'PM':1, 'ATK':{}, 'Status':'Normal', 'dano': '', 'Ação':ActPlayer, 'Tipo': 'Humano'}
+    global score
+    global fim_de_jogo
+    PLAYER = {'Nome': 'jogador', 'F':0, 'H':0, 'R':0, 'A':0, 'PdF':0, 'PV':1, 'PM':1, 'ATK':{}, 'Status':'Normal', 'dano': '', 'Ação':ActPlayer, 'Tipo': 'Humano', 'score':0}
     rolar = lambda: randint(1,6)
     testar = (lambda x: rolar() <= x)
     
     def MenuClasse():
         nonlocal PLAYER
         while True:
-            comando = input('Ver(v/ver), Escolher(e/escolher).\n').lower()
-            while comando.startswith('v'):
-                print ('Estão disponíveis as seguintes classes:')
-                classe = input('Arqueiro(a/arqueiro), Guerreiro (g/guerreiro), Mago(m/mago), Paladino (p/paladino), voltar(v/voltar).\n').lower()
-                if classe.startswith('a'):
-                    print ('Um antigo ditado diz que um arqueiro carrega um número de vidas em sua aljava, tamanha é a sua precisão.\n\
-Conhecidos por serem silenciosos e mortais, os arqueiros geralmente são notados apenas quando a flecha atinge o alvo.')
-                    print ('Você terá Força 0, Habilidade 2, Resistência 2, Armadura 1, Poder de Fogo 3\n\
-10 PVs, 10 PMs e pode atacar com seu arco.\n\
-Além disso, você pode realizar um poderoso disparo que tem mais chances de causar dano crítico e que,\n\
-quando o faz, causa mais dano que os ataques normais ao custo de (4 PMs)')
-                elif classe.startswith('g'):
-                    print('Vindo diretamente das arenas de gladiadores, você parte pelo mundo em busca\n\
-de provar que é o melhor guerreiro de toda Arton, e que nada poderá superar sua habilidade com a Espada')
-                    print('Você terá Força 3, Habilidade 2, Resistência 3, Armadura 2, Poder de Fogo 0\n\
-20 PVs, 10 PMs e além de poder atacar com seu machado, poderá também usar um poderoso golpe que causa dano crítico\n\
-muaior.')
-                elif classe.startswith('m'):
-                    print('Armado de um grande cajado e de um extenso grimório, você tem como objetivo provar\n\
-que é capaz de dominar a magia em todos os seus aspectos.')
-                    print('Você terá Força 1, Habilidade 3, Resistência 2, Armadura 0  e Poder de Fogo 0\n\
-10 PVs, 30 PMs e será capaz de atacar com seu cajado, além das seguintes magias:\n\
-Arpão (15 PMs): uma poderosa onda sônica de energia mágica capaz de obliterar um inimigo:\n\
-Bola de fogo (6 PMs): uma bola de fogo que explode em uma área, atingindo até três inimigos;\n\
-O Crânio Voador de Vladislav (3 PMs): um crânio mágico que explode contra um alvo, ignorando sua Armadura.')
-                elif classe.startswith('p'):
-                    print('Os deuses escolheram você para cumprir uma missão divina na terra. Através das palavras de sua divindade, de sua coragem\n\
-e sua força, você espalha a palavra de seu patrono pelo mundo, cumprindo as missões\n\
-que lhe são dadas.')
-                    print('Você terá Força 2, Habilidade 3, Resistência 3, Armadura 2, Poder de Fogo 0\n\
-15 PVs, 15 PMs e poderá usar sua Espada para ataques simples. Além disso, terá a sua disposição\n\
-as seguintes magias:\n\
-Ataque Vorpal (1 PM): Um ataque que, em caso de acerto crítico, pode decapitar um inimigo.\n\
-Cura mágica (2 PMs): você recupera entre 1 e 6 PVs próprios.\n\
-')
-                else:
+            print('...')
+            comando = input('Arqueiro(a/arqueiro), Guerreiro(g/guerreiro), Mago(m/mago), Paladino(p/paladino).\n...\n').lower()
+            print('...')
+            if comando.startswith('a'):
+                print ('<Arkam Braço Metálico> -- Um antigo ditado diz que um arqueiro carrega um número de vidas em sua aljava, tamanha é a sua precisão.\n'
+                       '<Arkam Braço Metálico> Conhecidos por serem silenciosos e mortais, um bom arqueiro só é notado depois que sua fleche atinge seu alvo.')
+                print ('<!> Você terá Força 0, Habilidade 2, Resistência 2, Armadura 1, Poder de Fogo 3\n'
+                       '<!> 10 PVs, 10 PMs e pode atacar com seu arco.\n'
+                       '<!> Além disso, você pode realizar um poderoso disparo que tem mais chances de causar dano crítico e que, quando o faz, causa mais dano que os ataques normais ao custo de 4 PMs.')
+                print ('...')
+                comando = input('Confirmar (c/confirmar), voltar(v/voltar)').lower()
+                if comando.startswith('c'):
+                    PLAYER['F'] = 1
+                    PLAYER['H'] = 2
+                    PLAYER['R'] = 2
+                    PLAYER['A'] = 1
+                    PLAYER['PdF'] = 3
+                    PLAYER['PV'] = 10
+                    PLAYER['PM'] = 10
+                    PLAYER['MAX'] = (10, 10)
+                    PLAYER['dano'] = 'seu arco'
+                    PLAYER['ATK'] = {'Arco':{'ATK':Disparar, 'PM':0}, 'Faca':{'ATK':Atacar, 'PM':0},'Golpe Fatal':{'ATK':TiroCerteiro, 'PM':4}}
                     break
-            while comando.startswith('e'):
-                classe = input ('Escolha sua classe: Arqueiro(a/arqueiro), Guerreiro(g/guerreiro), Mago(m/mago), Paladino (p/paladino), Voltar(v/voltar)').lower()
-                if classe.startswith('g'):
+                elif comando.startswith('v'):
+                    continue
+                else:
+                    print('...')
+                    print('<Arkam Braço Metálico> -- Não entendi. Pode repetir?')
+            elif comando.startswith('g'):
+                print('<Arkam Braço Metálico> -- Os guerreiros costumam ser a alma de um grupo de aventureiros.\n'
+                      '<Arkam Braço Metálico> A força e bravura de um guerreiro pode ser a diferença entre encontrar um tesouro ou ir para ao encontro dos deuses.')
+                print('<!> Você terá Força 3, Habilidade 2, Resistência 3, Armadura 2, Poder de Fogo 0\n'
+                      '<!> 20 PVs, 10 PMs e usará um machado.\n'
+                      '<!> Além disso, você poderá também usar um poderoso golpe que causa muito mais dano por 3 PMs.')
+                print('...')
+                comando = input('Confirmar (c/confirmar), voltar(v/voltar)').lower()
+                if comando.startswith('c'):
                     PLAYER['F'] = 3
                     PLAYER['H'] = 2
                     PLAYER['R'] = 3
@@ -88,40 +88,65 @@ Cura mágica (2 PMs): você recupera entre 1 e 6 PVs próprios.\n\
                     PLAYER['MAX'] = (20, 10)
                     PLAYER['dano'] = 'seu machado'
                     PLAYER['ATK'] = {'Machado':{'ATK':Atacar, 'PM': 0}, 'Golpe Demolidor':{}}
-                    return
-                elif classe.startswith('m'):
+                    break
+                elif comando.startswith('v'):
+                    continue
+                else:
+                    print('...')
+                    print('<Arkam Braço Metálico> -- Não entendi. Pode repetir?')
+            elif comando.startswith('m'):
+                print('<Arkam Braço Metálico> -- Em todo lugar de Arton, grandes histórias são contadas sobre magos e seus poderes.'
+                      '<Arkam Braço Metálico> -- Com certeza ser um mago requer inteligencia e é uma tarefa difícil... mas nao sei se a masmorra é um bom lugar pra você.')
+                print('<!> Você terá Força 1, Habilidade 3, Resistência 2, Armadura 0  e Poder de Fogo 0\n'
+                      '<!> 10 PVs, 30 PMs e será capaz de atacar com seu cajado, além das seguintes magias:\n'
+                      '<!> Arpão (15 PMs): uma poderosa onda sônica de energia mágica capaz de obliterar um inimigo:\n'
+                      '<!> Bola de fogo (6 PMs): uma bola de fogo que explode em uma área, atingindo até três inimigos;\n'
+                      '<!> O Crânio Voador de Vladislav (3 PMs): um crânio mágico que explode contra um alvo, ignorando sua Armadura.')
+                print('...')
+                comando = input('Confirmar (c/confirmar), voltar(v/voltar)').lower()
+                if comando.startswith('c'):
                     PLAYER['F'] = 1
                     PLAYER['H'] = 3
                     PLAYER['R'] = 2
-                    PLAYER['A'] = 0
+                    PLAYER['A'] = 1
                     PLAYER['PV'] = 10
                     PLAYER['PM'] = 30
                     PLAYER['MAX'] = (10, 30)
                     PLAYER['dano'] = 'seu cajado'
                     PLAYER['ATK'] = {'Arpão':{'ATK':Arpao, 'PM':15},'Cajado': {'ATK':Atacar, 'PM': 0}, 'Bola de Fogo': {'ATK': BolaDeFogo, 'PM': 5}}
-                    return
-                elif classe.startswith('a'):
-                    PLAYER['H'] = 2
-                    PLAYER['R'] = 2
-                    PLAYER['A'] = 1
-                    PLAYER['PdF'] = 3
-                    PLAYER['PV'] = 10
-                    PLAYER['PM'] = 10
-                    PLAYER['MAX'] = (10, 10)
-                    PLAYER['dano'] = 'seu arco'
-                    PLAYER['ATK'] = {'Arco':{'ATK':Disparar, 'PM':0}, 'Golpe Fatal':{'ATK':TiroCerteiro, 'PM':4}}
-                    return
-                elif classe.startswith('p'):
+                    break
+                elif comando.startswith('v'):
+                    continue
+                else:
+                    print('...')
+                    print('<Arkam Braço Metálico> -- Não entendi. Pode repetir?')
+            elif comando.startswith('p'):
+                print('<Arkam Braço Metálico> -- Os deuses escolheram você para cumprir uma missão divina em Arton.\n'
+                      '<Arkam Braço Metálico> -- Mas será que suas orações serão suficientes para fazê-lo vencer esse desafio? É o que vamos descobrir.')
+                print('<!> Você terá Força 2, Habilidade 2, Resistência 3, Armadura 2, Poder de Fogo 0\n'
+                      '<!> 15 PVs, 15 PMs e poderá usar sua Espada para ataques simples. Além disso, terá a sua disposição as seguintes magias:\n'
+                      '<!> Ataque Vorpal (3 PMs): um poderoso ataque que, em caso de acerto crítico, pode decapitar um inimigo.\n'
+                      '<!> Cura mágica (2 PMs): você recupera entre 1 e 6 PVs próprios.\n'
+                      '<!> Esconjuro (5 PMs): pode ser usada apenas contra mortos-vivos. Caso tenha sucesso, sua magia bane o alvo, destruindo de uma so vez.')
+                print('...')
+                comando = input('Confirmar (c/confirmar), voltar(v/voltar)').lower()
+                if comando.startswith('c'):
                     PLAYER['F'] = 2
-                    PLAYER['H'] = 3
+                    PLAYER['H'] = 2
                     PLAYER['R'] = 3
                     PLAYER['A'] = 2
-                    PLAYER['PV']= 15
+                    PLAYER['PV'] = 15
                     PLAYER['PM'] = 15
                     PLAYER['MAX'] = (15, 15)
                     PLAYER['dano'] = 'sua espada'
                     PLAYER['ATK'] = {'Espada':{'ATK':Atacar, 'PM':0}, 'Cura Mágica':{'ATK':CuraMagica, 'PM':2}, 'Esconjuro':{'ATK': Esconjuro, 'PM': 5}, 'Ataque Vorpal':{'ATK':AtaqueVorpal, 'PM': 3}}
-
+                    break
+                elif comando.startswith('v'):
+                    continue
+                else:
+                    print('...')
+                    print('<Arkam Braço Metálico> -- Não entendi. Pode repetir?')
+                    
     def Atacar(atacante, vitima):
         '''
         Função definida para os ataques fisicos simples, feitos com F.
@@ -223,7 +248,7 @@ sofrendo {} pontos de dano.'.format(vitima['Nome'], dano))
         nonlocal testar
         atacante['PM'] -= 5
         msg ='Depois de uma oração, {} estende as mãos na direção de {}'.format(atacante['Nome'], vitima['Nome'])
-        if (testar(vitima['R'])) or (vitima['Tipo'] != 'Morto-vivo'):
+        if (testar(vitima['R'])) or (vitima['Tipo'] != 'morto-vivo'):
             msg+=', mas nada acontece.'
         else:
             msg+=', liberando uma grande quantidade de luz que atinge seu alvo em cheio, destruindo-o.'
@@ -353,6 +378,67 @@ sofrendo {} pontos de dano.'.format(vitima['Nome'], dano))
             vitima['PV'] -= dano
         print(msg)
 
-    MenuClasse()
-    PLAYER['Nome'] = input('Qual o seu nome?\n')
-    return PLAYER
+    def EncontrarInimigo():  #IA de controle de inimigos
+        print('...')
+        print('<!>')
+
+    def Jogar():
+        '''
+        Função definida para o Menu entre as batalhas.
+        '''
+        global fim_de_jogo
+        while True:
+            print('...')
+            comando = input('Continuar(c/continuar), Salvar(s/salvar), Ajuda(a/ajuda), Gastar Experiência (e/exp), Desistir(d/desistir)\n')
+            if comando.startswith('c'):
+                break
+            elif comando.startswith ('s'):  # Menu de Salvar, lembrar de configurar
+                pass
+            elif comando.startswith ('a'):  # Instruções da masmorra
+                pass
+            elif comando.startswith('e'):  # Menu de Experiência
+                pass
+            elif comando.startswith('d'):
+                fim_de_jogo = True
+                break
+            else:
+                print('...')
+                print('Escolha um comando válido.')
+            
+    #  Fichas dos inimigos abaixo:
+    ZUMBI = {'Nome': 'Zumbi', 'F':1, 'H':2, 'R':2, 'A':0, 'PdF':0, 'PV':10, 'PM':10, 'Status':'Normal', 'dano': 'suas garras', 'Ação':Grunt, 'Tipo': 'morto-vivo', 'score':5}
+    ZUMBI['ATK'] = {'ataque': {'ATK': Atacar, 'PM': 0}}
+    ZUMBI['Morte'] = ''
+
+    # Inicio do Jogo
+    while True:
+        escolha = input('Novo Jogo(n/novo), Continuar(c/continuar), Sair(s/sair)\n').lower()
+        if escolha.startswith('c'):
+            pass
+        elif escolha.startswith('s'):
+            break
+        elif escolha.startswith('n'):
+            print('<!> Bem-vindo, aventureiro, ao desafio da masmorra do protetorado do reino.\n\n\n')
+            PLAYER['Nome'] = input('<Arkam Braço Metálico> -- Então você acha que é capaz de vencer o desafio? Bom, isso é o que vamos ver.\n'
+                                   '<Arkam Braço Metálico> -- Antes de entrar, diga-me, qual seu nome?\n...\n').capitalize()
+            print('...')
+            print('<Arkam Braço Metálico>: -- Bem-vindo, {}. A masmorra está pronta para recebê-lo, mas pelo visto você ainda não está pronto para prosseguir.\n'
+                  '*Arkam lhe aponta uma mesa com armas e equipamentos de diversos tipos.*'
+                  '\n<Arkam Braço Metálico> -- Na masmorra você precisa usar equipamentos especiais, feitos para as mais diversas classes de aventureiros. Escolha com sabedoria.'.format(PLAYER['Nome']))
+            MenuClasse()
+            print('...')
+            print('<!>Arkam permite que você pegue seus equipamentos e o guia através de um corredor escuro até uma porta de madeira velha.\n'
+                  '<!> A porta está protegida por uma barra de metal brilhante, provavelmente mágico, que emite uma fraca luz azul.')
+            print('<Arkam Braço Metálico> -- É aqui que nos separamos, {}. Espero encontrá-lo do outro lado da masmorra. Hahaha'
+                  '<Arkam Braço Metálico> -- Boa sorte.'.format(PLAYER['Nome']))
+            print('<!> A porta se abre e você a entra, ouvindo o rangido e a batida da porta fechando-se logo atrás de você.')
+            while not fim_de_jogo:
+                Jogar()
+                if not fim_de_jogo:
+                    inimigos = EncontrarInimigo()
+                    print('...')
+                    Combate(PLAYER, inimigos)
+        else:
+            print('<!> Comando inválido. Tente novamente.')
+
+Main()
